0add976bf6cd0a84458144894a29870aeb190851
Erik <eknyquist@gmail.com>
2020-01-11 17:39:59 -0800

    hashtables.c: add some API docs

--- source/common/hashtables.c
+++ source/common/hashtables.c
@@ -1,11 +1,20 @@
+/**
+ * Helper functions to create hashtables with different configurations
+ */
+
 #include <stdint.h>
 #include "hashtables_api.h"
+
 
 uint8_t _pointer_comparison_func(char *str1, char *str2)
 {
     return (str1 == str2);
 }
 
+
+/**
+ * @see hashtables_api.h
+ */
 hashtable_status_e create_string_comparison_hashtable(hashtable_t *table,
                                                       size_t data_size_bytes)
 {
@@ -19,6 +28,9 @@ hashtable_status_e create_string_comparison_hashtable(hashtable_t *table,
 }
 
 
+/**
+ * @see hashtables_api.h
+ */
 hashtable_status_e create_pointer_comparison_hashtable(hashtable_t *table,
                                                        size_t data_size_bytes)
 {
--- source/common/hashtables_api.h
+++ source/common/hashtables_api.h
@@ -1,13 +1,45 @@
+/**
+ * Helper functions to create hashtables with different configurations
+ */
+
 #ifndef HASHTABLES_API_H
 #define HASHTABLES_API_H
 
 #include "hashtable_api.h"
 
 
+/**
+ * Create a hashtable that compares string keys by comparing every character
+ * of both keys.
+ *
+ * @param    table            Pointer to hashtable_t structure to initialize
+ * @param    data_size_bytes  Size of single hashtable entry (in bytes)
+ *
+ * @return   Same as hashtable_create
+ */
 hashtable_status_e create_string_comparison_hashtable(hashtable_t *table,
                                                       size_t data_size_bytes);
 
 
+/**
+ * Create a hashtable that compares string keys by comparing the string data
+ * pointers of both keys. When using this type of hashtable, ensure only cached
+ * strings are used for string keys when adding new entries (call
+ * string_cache_add on the string first).
+ *
+ * The advantage of this type of of hashtable is that insertions/deletions are
+ * faster, since string comparisons only require a single pointer comparison,
+ * regardless of the length of either string key. In order for this comparison
+ * to be meaningful, however, we need a gaurantee that unique pointers point
+ * to unique strings (in other words, that no duplicate strings exist in the
+ * pool we draw our string keys from). This is the purpose of the string_cache
+ * module.
+ *
+ * @param    table            Pointer to hashtable_t structure to initialize
+ * @param    data_size_bytes  Size of single hashtable entry (in bytes)
+ *
+ * @return   Same as hashtable_create
+ */
 hashtable_status_e create_pointer_comparison_hashtable(hashtable_t *table,
                                                        size_t data_size_bytes);
 
